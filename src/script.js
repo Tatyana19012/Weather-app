879158363655c791efbd645f747b4cb2f956dae1
Tatyana19012 <chichika@ukr.net>
2022-08-20 17:38:12 +0200

    Update search

--- src/script.js
+++ src/script.js
@@ -86,4 +86,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-searchCity("New York");
+search("New York");
