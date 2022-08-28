21e4a3aefa60dd4a146028e79e3208cadcb05152
Tatyana19012 <chichika@ukr.net>
2022-08-28 23:25:28 +0200

    Apdate API

--- src/script.js
+++ src/script.js
@@ -23,26 +23,29 @@ function formatDate(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
-                <img src="" alt="" width="42" />
+                <div class="weather-forecast-date">${forecastDay.dt}</div>
+                <img
+                src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+                alt="" 
+                width="42"/>
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperature-max">18º</span>
-                  <span class="weather-forecast-temperature-min">12º</span>
+                  <span class="weather-forecast-temperature-max">${forecastDay.temp.max}º</span>
+                  <span class="weather-forecast-temperature-min">${forecastDay.temp.min}º</span>
                 </div>
               </div>
             `;
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
